8d432f1a22cf7e8a9e1d87a216f5871a57f7b520
Fabiano <fabiano.novais.99@gmail.com>
2018-09-15 02:56:57 -0300

    Exercício lightsaber feito

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+let paginaPergunta = document.querySelector ("#pagina-pergunta");
+let paginaSabre = document.querySelector("#pagina-sabre");
+let botaoSith = document.querySelector(".sith");
+let botaoJedi = document.querySelector(".jedi");
+let lamina = document.querySelector(".lamina");
+
+function escolherSith(){
+    paginaPergunta.style.display="none";
+    paginaSabre.style.display="flex";   
+}
+
+function escolherJedi(){
+    paginaPergunta.style.display="none";
+    paginaSabre.style.display="flex";
+    lamina.style.background = "linear-gradient(90deg, #049c04, #9ce99c, #049c04)";
+    lamina.style.boxShadow = "0 0 50px #049c04";   
+}
+
+function alternarSabre(){
+    if(lamina.style.opacity ==1){
+    lamina.style.opacity=0;
+    }
+    else{
+    lamina.style.opacity=1;
+    }
+}
+
+botaoSith.onclick = escolherSith;
+botaoJedi.onclick = escolherJedi;
+paginaSabre.onclick = alternarSabre;
+
+
+
+
+
 // Registra o service worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
